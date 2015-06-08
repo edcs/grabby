8023e64b0e1ae315ac8cd1d8255d1c4e2209a453
Edward Coleridge Smith <edward.smith@pod-point.com>
2015-06-08 15:18:05 +0100

    Updated PhantomJS script to accept arguments

--- grabby.js
+++ grabby.js
@@ -1,15 +1,14 @@
-// PhantomJS Screenshot Capture...
-var fs = require('fs'),
-    args = require('system').args,
+var args = require('system').args,
     page = require('webpage').create();
 
-// Read the HTML view into the page...
-page.content = fs.read(args[1]);
-page.viewportSize = {width: 600, height: 600};
-page.paperSize = {format: 'A4', orientation: 'portrait', margin: '1cm'};
+console.log(args);
 
-// Give the page 250ms to render any images...
-window.setTimeout(function() {
-    page.render(args[1]);
+page.viewportSize = {
+    width: args[3],
+    height: args[4]
+}
+
+page.open(args[1], function() {
+    page.render(args[2]);
     phantom.exit();
-}, 150);
+});
