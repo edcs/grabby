ed5915c84d07380d8fa4c0f984446b05abb5888b
Edward Coleridge Smith <edward.smith@pod-point.com>
2016-06-28 11:41:54 +0100

    Allow custom web page config

--- grabby.js
+++ grabby.js
@@ -1,13 +1,27 @@
 var args = require('system').args,
+    each = require('@pod-point/utils').each,
     page = require('webpage').create();
 
-console.log(args);
+/**
+ * PhantomJS config object is passed to this script using a URL encoded JSON string. This needs to be decoded and parsed
+ * into a JavaScript object.
+ */
+var config  = JSON.parse(decodeURIComponent(args[3]));
 
-page.viewportSize = {
-    width: args[3],
-    height: args[4]
-}
+/**
+ * We then loop through all the object properties and append them to the PhantomJS webpage module. For more information
+ * about possible config options, check out the PhantomJS docs:
+ *
+ * @link http://phantomjs.org/api/webpage/
+ */
+each(config, function (value, key) {
+    page[key] = value;
+});
 
+/**
+ * We then open up the webpage via the supplied argument and render it to the location specified in the second argument.
+ * Once this process is complete we exit PhantomJS.
+ */
 page.open(args[1], function() {
     page.render(args[2]);
     phantom.exit();
