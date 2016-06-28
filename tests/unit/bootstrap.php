93a6036c56a566049a32b1824b5ed4a9fc9c903e
Edward Coleridge Smith <edward.smith@pod-point.com>
2016-06-28 09:08:08 +0100

    Remove aspect mock

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -1,14 +1,3 @@
 <?php
 
-// Turn on all errors.
-error_reporting(E_ALL);
-
-// Require Composer autoloader.
 require dirname(dirname(__DIR__)).'/vendor/autoload.php';
-
-// Setup AspectMock
-$kernel = \AspectMock\Kernel::getInstance();
-$kernel->init([
-    'debug' => true,
-    'includePaths' => [__DIR__.'/../src']
-]);
